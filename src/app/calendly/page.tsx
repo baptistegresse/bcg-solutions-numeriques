431e0b9edaa07f5cfa98cf2647d3eddc0b1b3f5e
baptiste gresse <baptiste5gresse@gmail.com>
2025-07-01 07:07:55 +0200

    Add react-calendly integration and create Calendly page component

---
+++ src/app/calendly/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { InlineWidget } from "react-calendly";
+
+
+export default function CalendlyPage() {
+
+  const params = useSearchParams();
+  const project_id = params.get('project_id') || '';
+  const calendlyBaseUrl = "https://calendly.com/bcg2180/30min";
+
+
+  return (
+    <div>
+      <h1>Prise de rendez-vous</h1>
+      <InlineWidget
+        url={calendlyBaseUrl}
+        prefill={{
+          customAnswers: {
+            a1: project_id,
+          }
+        }}
+      />
+    </div>
+  );
+}
